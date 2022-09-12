08fc4cea8fba345dc9591d235ae0122c79202342
Dhanraj balkrishna shetty <dhanrajCoding@gmail.com>
2022-09-12 11:24:13 +0530

    updated app.js

--- navbar1/src/App.js
+++ navbar1/src/App.js
@@ -1,8 +1,25 @@
-import './App.css';
+import React, { useState } from "react";
+import { Button } from "./components/Button";
+import { Link } from "react-router-dom";
+import './Navbar.css'
+import Dropdown from "./components/Dropdown";
 
-function App() {
+function Navbar() {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
   return (
-    <h1>hello</h1>
+    <>
+      <nav className="navbar">
+        <Link to="/" className="navbar-logo">
+          Star Instruments & Control
+        </Link>
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? "fas fa-times" : "fas-fa-bars"} />
+        </div>
+      </nav>
+    </>
   );
 }
 
--- navbar1/src/components/Navbar.js
+++ navbar1/src/components/Navbar.js
@@ -1,17 +1,19 @@
-import React, { useStat } from "react";
-import { Button } from "./Button";
+// import React, { useState } from "react";
+// import { Button } from "./Button";
 import "./Navbar.css";
-import Dropdown from "./Dropdown";
+// import Dropdown from "./Dropdown";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <>
       <nav className="navbar">
-        <link to="/" className="navabar-logo">
-          Star Instruments
-        </link>
+        <Link to="/" className="navabar-logo">
+          Star Instruments & Control
+        </Link>
       </nav>
     </>
   );
 }
+
+export default Navbar;
